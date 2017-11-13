c6668b8b8bb1bef97f8a4c32f057b433ba5ae88e
Arthur LORENT <arthur@acid.fr>
2017-11-13 16:42:40 +0100

    fixed button wrong translations

--- views/components/buttons/cancel.blade.php
+++ views/components/buttons/cancel.blade.php
@@ -2,5 +2,5 @@
         class="{!! config('tablelist.template.button.cancel.class') !!}"
         data-dismiss="modal">
     {!! config('tablelist.template.button.cancel.icon') !!}
-    {{ trans('tablelist::tablelist.modal.cancel') }}
+    {{ trans('tablelist::tablelist.modal.action.cancel') }}
 </button>
